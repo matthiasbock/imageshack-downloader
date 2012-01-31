03cfa63e7ccda2917fd801ae251f5154bd539cbb
Aquaman <noreply@anonymous>
2012-01-31 17:38:08 +0000

    test script

--- imageshack.py
+++ imageshack.py
@@ -8,13 +8,21 @@ import memcache
 import pickle
 from math import ceil
 
+def rand():
+	return '0.'+''.join([str(random.randint(0,9)) for i in range(16)])	# 0.xxxxxxxxxxxxxxxx
+
 class ImageShack:
 	def __init__(self, username=None, password=None):
+		self.reset()
 		self.username = username
 		self.password = password
-		self.r = Robot(debug=True)
-		self.logged_in = False
+
+	def reset(self):
 		random.seed()
+		self.r = Robot(debug=False)
+		self.logged_in = False
+		self.got_image_list = False
+		self.got_tag_list = False
 
 	def login(self, username=None, password=None):
 		if username is None or password is None:
@@ -24,25 +32,29 @@ class ImageShack:
 			return False
 
 		self.r.POST('imageshack.us/auth.php', {'stay_logged_in':'true', 'format':'json', 'username':username, 'password':password})
-		print str(self.r.Page)
-		self.logged_in = True
-		return True
 
-	def get_picture_list(self, ipage, tagid=-1):
+		import simplejson
+		json = simplejson.loads(str(self.r.Page))
+		self.logged_in = json['status'] is True
+		return self.logged_in
+
+	def download_image_list(self, ipage=1, tagid=-1):
 		if not self.logged_in:
 			if not self.login():
 				return False
 
-		rand = '0.'+''.join([str(random.randint(0,9)) for i in range(16)])
-		self.r.GET('my.imageshack.us/images.php?ipage='+str(ipage)+'&tagid='+str(tagid)+'&rand='+rand)
+		self.r.GET('my.imageshack.us/images.php?ipage='+str(ipage)+'&tagid='+str(tagid)+'&rand='+rand() )
 
-		self.inumpages = int(between(self.r.Page, '<input type="hidden" id="inumpages" value="', '"'))		# pages
-		self.inumitems = int(between(self.r.Page, '<input type="hidden" id="inumitems" value="', '"'))		# images
+		self.inumpages = int(between(self.r.Page, '<input type="hidden" id="inumpages" value="', '"'))		# image page count
+		self.inumitems = int(between(self.r.Page, '<input type="hidden" id="inumitems" value="', '"'))		# image count
+		self.inumcached = int(between(self.r.Page, '<input type="hidden" id="inumcached" value="', '"'))	# number of images listed on current page
+
+# values below are available but not used in the code, maybe at some pointer later
 #		self.istartpage = int(between(self.r.Page, '<input type="hidden" id="istartpage" value="', '"'))	# 1
 #		self.iendpage = int(between(self.r.Page, '<input type="hidden" id="iendpage" value="', '"'))		# page until which the cache works
-		self.inumcached = int(between(self.r.Page, '<input type="hidden" id="inumcached" value="', '"'))	# number of images listed
 
 		mc = memcache.Client(['127.0.0.1:11211'], debug=1)
+		filenames = []
 		for i in range(self.inumcached):
 			div = between(self.r.Page, '<div id="i'+str(i)+'">', '</div></div></div></div>', include_after=True)
 			image = {
@@ -55,10 +67,20 @@ class ImageShack:
 				'tags'		: between(div, '<div class="tags">', '</div></div></div>')
 				}
 			mc.set(image['filename'], pickle.dumps(image))
-#			print pickle.loads(mc.get(image['filename']))['date']
+#			print pickle.loads(mc.get(image['filename']))['date']	# for debugging: read from memcache, what we just stored there
+			filenames.append( image['filename'] )
+		mc.set('imageshackfs-'+self.username+'-filenames', pickle.dumps(filenames))
 		del mc
+		self.got_image_list = True
 
-	def get_tag_list(self):
+	def get_image_list(self, ipage=1, tagid=-1):
+		if not self.got_image_list:
+			self.download_image_list(ipage, tagid)
+
+		mc = memcache.Client(['127.0.0.1:11211'], debug=1)
+		return pickle.loads( mc.get('imageshackfs-'+self.username+'-filenames') )
+
+	def download_tag_list(self):
 		if not self.logged_in:
 			if not self.login():
 				return False
@@ -76,11 +98,19 @@ class ImageShack:
 			tags.append(tag)
 
 		mc = memcache.Client(['127.0.0.1:11211'], debug=1)
-		mc.set('imageshack-tags', pickle.dumps(tags))
-		print pickle.loads(mc.get('imageshack-tags'))
+		mc.set('imageshackfs-'+self.username+'-tags', pickle.dumps(tags))
+#		print pickle.loads(mc.get('imageshack-tags'))	# for debugging: read from memcache, what we just stored there
 		del mc
+		self.got_tag_list = True
 
-	def get_album_list(self):
+	def get_tag_list(self):
+		if not self.got_tag_list:
+			self.download_tag_list()
+
+		mc = memcache.Client(['127.0.0.1:11211'], debug=1)
+		return pickle.loads( mc.get('imageshackfs-'+self.username+'-tags') )
+
+	def download_album_list(self):
 		if not self.logged_in:
 			if not self.login():
 				return False
@@ -103,9 +133,11 @@ class ImageShack:
 				'preview'	: between(div, '<div class="ign">', '</div>')
 				}
 			mc.set(gallery['link'], pickle.dumps(gallery))
-			print pickle.loads(mc.get(gallery['link']))['title']
+#			print pickle.loads(mc.get(gallery['link']))['title'] # for debugging: read from memcache, what we just stored there
 		del mc
 
+	def create_album(self, name):
+		return
 
 	def __del__(self):
 		self.r.GET('my.imageshack.us/logout.php')
--- test.py
+++ test.py
@@ -6,5 +6,14 @@ username, password = loadconfig()
 
 from imageshack import ImageShack
 
+print 'Login ...'
 i = ImageShack(username, password)
-i.get_album_list()
+
+#print 'Requesting image list ...'
+#l = i.get_image_list()
+#print l
+#print len(l)
+
+print 'Requesting tag list ...'
+print i.get_tag_list()
+
