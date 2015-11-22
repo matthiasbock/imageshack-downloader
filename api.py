374ccd95273b13bb8e4aa9d5d9663fdadc556dab
Matthias <mail@matthiasbock.net>
2015-11-22 18:42:26 +0100

    Login via API works

--- api.py
+++ api.py
@@ -1,158 +1,177 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
 
+# config file parser
 from ConfigParser import RawConfigParser
 
+# HTTP client
 from httpclient import HttpClient
 from htmlparser import between
 
-def loadconfig():
-	parser = RawConfigParser()
-	parser.read('imageshack.conf')
-	username = parser.get('Login', 'Username')
-	password = parser.get('Login', 'Password')
-	del parser
+# JSON parser
+from simplejson import loads,dumps
 
-	return username, password
-
+#
+# Object to hold authentication token
+# and provide methods to access imageshack.com
+#
 class ImageShack_Account:
-	def __init__(self):
-		self.r = HttpClient(debug=False)
-		self.logged_in = False
-		self.Images = None
-		self.Tags = None
-		self.Albums = None
 
-	def login(self, username, password):
-		# GET is sufficient
-		#self.r.GET('imageshack.us/auth.php?username='+username+'&password'+password)
+    def __init__(self):
+        self.client = HttpClient(debug=False)
+        self.logged_in = False
+        self.images = None
+        self.albums = None
+        self.tags = None
 
-		# but we use POST, to check, if login was successfull
-		self.r.POST('imageshack.us/auth.php', {'stay_logged_in':'true', 'format':'json', 'username':username, 'password':password})
+    def authenticate(self):
+        #
+        # Open imageshack.conf
+        # and read username and password
+        # to use as login credentials 
+        #
+        parser = RawConfigParser()
+        parser.read('imageshack.conf')
+        username = parser.get('Login', 'Username')
+        password = parser.get('Login', 'Password')
+        del parser
 
-		import simplejson
-		json = simplejson.loads(str(self.r.Page))
-		self.logged_in = json['status'] is True
-		
-		if self.logged_in:
-			self.get_number_of_pages()
-		
-		return self.logged_in
+        # POSTing requires an API key
+        api_key = open("api_key.txt").read()
 
-	def __del__(self):
-		try:
-			self.r.GET('my.imageshack.us/logout.php')
-		except:
-			pass
+        #
+        # Authentication:
+        # POST to https://api.imageshack.com/v2/user/login
+        # see http://api.imageshack.us/
+        #
+        params = {
+                    'api_key'       : api_key,
+                    'username'      : username,
+                    'password'      : password,
+                    'set_cookies'   : '1',
+                    'remember_me'   : '1'
+                 }
+        self.client.POST('https://api.imageshack.com/v2/user/login', params)
 
+        print str(self.client.Page)
 
-	# Images
+        # Parse response as JSON
+        json = loads(str(self.client.Page))
+        self.logged_in = json["success"] is True
 
-	def get_number_of_pages(self):
-		self.r.GET('my.imageshack.us/images.php?ipage=1')
+        # Extract username from response
+        # In the config file it could be the email address
+        self.username = json["result"]["username"]
+        
+        return self.logged_in
 
-		# number of image pages
-		self.pages = int(between(self.r.Page, '<input type="hidden" id="inumpages" value="', '"'))
-		# number of images in total
-		self.images = int(between(self.r.Page, '<input type="hidden" id="inumitems" value="', '"'))
+    #
+    # Images
+    #
+    def get_user_images(self):
+        #GET https://api.imageshack.com/v2/user/<username>/images
+        return
+        
+        
+    def get_number_of_pages(self):
+        self.client.GET('my.imageshack.us/images.php?ipage=1')
 
-	def get_images_on_page(self, ipage):
-		self.r.GET('my.imageshack.us/images.php?ipage='+str(ipage))
+        # number of image pages
+        self.pages = int(between(self.client.Page, '<input type="hidden" id="inumpages" value="', '"'))
+        # number of images in total
+        self.images = int(between(self.client.Page, '<input type="hidden" id="inumitems" value="', '"'))
 
-		# number of images on current page
-		current_page_images = int(between(self.r.Page, '<input type="hidden" id="inumcached" value="', '"'))
+    def get_images_on_page(self, ipage):
+        self.client.GET('my.imageshack.us/images.php?ipage='+str(ipage))
 
-		# parse divs
-		from parse_images import Image
+        # number of images on current page
+        current_page_images = int(between(self.client.Page, '<input type="hidden" id="inumcached" value="', '"'))
 
-		images = {}
-		for i in range(current_page_images):
-			# ix = id of image on current page
-			img = Image( between(self.r.Page, '<div id="i'+str(i)+'">', '</div></div></div></div>', include_after=True) )
+        # parse divs
+        from parse_images import Image
 
-			# img.id = global id of image
-			images[ img.id ] = img
+        images = {}
+        for i in range(current_page_images):
+            # ix = id of image on current page
+            img = Image( between(self.client.Page, '<div id="i'+str(i)+'">', '</div></div></div></div>', include_after=True) )
 
-		return images
+            # img.id = global id of image
+            images[ img.id ] = img
 
+        return images
 
-	# Tags
+    def upload_image(self, path):
+        return
 
-	def download_tag_list(self):
-		if not self.logged_in:
-			if not self.login():
-				return False
+    def delete_image(self, image):
+        return
 
-		self.r.GET('my.imageshack.us/v_images.php')
+    #
+    # Tags
+    #
+    def get_tag_list(self):
+        if self.Tags is None:
+            self.download_tag_list()
+        return self.Tags
+        
+    def download_tag_list(self):
+        if not self.logged_in:
+            if not self.login():
+                return False
 
-		self.tnumitems = int(between(self.r.Page, '<input type="hidden" id="tnumitems"    value="', '"'))
+        self.client.GET('my.imageshack.us/v_images.php')
 
-		self.Tags = []
-		tags_per_page = 20
-		for i in range(self.tnumitems):
-			page = i / tags_per_page
-			number = i % tags_per_page
-			#self.Tags.append(Tag(between(self.r.Page, '<input type="hidden" id="tn'+str(page)+'_'+str(number)+'" value="', '"'), self))
+        self.tnumitems = int(between(self.client.Page, '<input type="hidden" id="tnumitems"    value="', '"'))
 
-		self.got_tag_list = True
+        self.Tags = []
+        tags_per_page = 20
+        for i in range(self.tnumitems):
+            page = i / tags_per_page
+            number = i % tags_per_page
+            #self.Tags.append(Tag(between(self.client.Page, '<input type="hidden" id="tn'+str(page)+'_'+str(number)+'" value="', '"'), self))
 
-	def get_tag_list(self):
-		if self.Tags is None:
-			self.download_tag_list()
-		return self.Tags
+        self.got_tag_list = True
 
-	# Albums
+    def add_tag(self, picture, tag):
+        return
 
-	def download_album_list(self):
-		if not self.logged_in:
-			if not self.login():
-				return False
-		
-		self.r.GET('my.imageshack.us/my_gallery/')
+    def delete_tag(self, picture, tag):
+        return
 
-		self.inumitems = int(between(self.r.Page, '<input type="hidden" id="inumitems" value="', '"'))		# galleries
-		self.inumcached = int(between(self.r.Page, '<input type="hidden" id="inumcached" value="', '"'))	# number of galleries listed
+    #
+    # Albums
+    #
+    def get_album_list(self):
+        if self.Albums is None:
+            self.download_album_list()
+        return self.Albums
 
-		self.Albums = []
-		#for i in range(self.inumcached):
-		#	self.Albums.append(Album(between(self.r.Page, '<div id="i'+str(i)+'">', '</div></div>', include_after=True), self))
-		self.got_album_list = True
+    def download_album_list(self):
+        if not self.logged_in:
+            if not self.login():
+                return False
+        
+        self.client.GET('my.imageshack.us/my_gallery/')
 
-	def get_album_list(self):
-		if self.Albums is None:
-			self.download_album_list()
-		return self.Albums
+        self.inumitems = int(between(self.client.Page, '<input type="hidden" id="inumitems" value="', '"'))        # galleries
+        self.inumcached = int(between(self.client.Page, '<input type="hidden" id="inumcached" value="', '"'))    # number of galleries listed
 
-	# Methods
+        self.Albums = []
+        #for i in range(self.inumcached):
+        #    self.Albums.append(Album(between(self.client.Page, '<div id="i'+str(i)+'">', '</div></div>', include_after=True), self))
+        self.got_album_list = True
+    
+    def create_album(self, name):
+        return
 
-	def upload_image(self, path):
-		return
+    def delete_album(self, album):
+        return
 
-	def delete_image(self, image):
-		return
+    def add_to_album(self, album, image):
+        return
 
-	def add_tag(self, picture, tag):
-		return
+    def delete_from_album(self, album, image):
+        return
 
-	def delete_tag(self, picture, tag):
-		return
-
-	def create_album(self, name):
-		return
-
-	def delete_album(self, album):
-		return
-
-	def add_to_album(self, album, image):
-		return
-
-	def delete_from_album(self, album, image):
-		return
-
-	def set_album_cover(self, album, image):
-		return
-
-	def change_sort_order(self, album, image, position):
-		return
-
-
+    def set_album_cover(self, album, image):
+        return
--- main.py
+++ main.py
@@ -1,56 +1,46 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
 
-from configparser import loadconfig
-username, password = loadconfig()
-
 from api import ImageShack_Account
+from sys import exit
+from os import mkdir
+from wget import wget
 
-print 'Login ...'
+print "Login ..."
 frog = ImageShack_Account()
-if frog.login(username, password):
-	print 'success'
+if frog.authenticate():
+    print "User "+frog.username+" successfully logged in."
 else:
-	print 'login failed'
-	from sys import exit
-	exit()
+    print "Error: Login failed."
+    exit()
+
+exit()
 
 print 'Number of images: '+str(frog.images)
 print 'Number of pages: '+str(frog.pages)
 
-# download all images
-from os import mkdir
-from wget import wget
-
-# all pages
+# iterate over all image pages
 try:
-	mkdir('images')
+    mkdir('images')
 except:
-	pass
+    pass
 for page in range(frog.pages):
-	# put in a separate folder
-	folder = 'page'+str(page+1).zfill(2)
-	print folder+' ...'
-#	try:
-#		mkdir(folder)
-#	except:
-#		pass
-	folder = 'images'
+    # put in a separate folder
+    folder = 'page'+str(page+1).zfill(2)
+    print folder+' ...'
+#    try:
+#        mkdir(folder)
+#    except:
+#        pass
+    folder = 'images'
 
-	# all images
-	images = frog.get_images_on_page(page+1)
-	processes = []
-	for i in images.keys():
-		processes.append( wget(images[i].url, folder+'/'+images[i].filename, frog.r.Cookies) )
-	print str(len(processes))+' download processes started. Waiting for all downloads to finish before continuing ...'
-	for p in processes:
-		p.wait()
+    # all images
+    images = frog.get_images_on_page(page+1)
+    processes = []
+    for i in images.keys():
+        processes.append( wget(images[i].url, folder+'/'+images[i].filename, frog.r.Cookies) )
+    print str(len(processes))+' download processes started. Waiting for all downloads to finish before continuing ...'
+    for p in processes:
+        p.wait()
 
 print 'completed.'
-
-#print 'Requesting album list ...'
-#albums = i.get_album_list()
-#print len(albums)
-#for album in albums:
-#	print album.title
-
